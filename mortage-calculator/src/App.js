203abbd87cd24a5b4cdd5f30d2977093efa2fa06
Sanjay <ksanjayk@protonmail.com>
2023-05-26 18:26:47 +0530

    layout

--- mortage-calculator/src/App.js
+++ mortage-calculator/src/App.js
@@ -1,3 +1,4 @@
+import { Container, Grid } from '@mui/material';
 import Navbar from './components/Navbar';
 import Result from './components/Result';
 import SliderSelect from './components/SliderSelect';
@@ -7,9 +8,17 @@ function App() {
   return (
     <div className='App'>
       <Navbar />
-      <SliderSelect />
-      <TenureSelect />
-      <Result/>
+      <Container maxWidth='xl' sx={{ mt: 4 }}>
+        <Grid container spacing={5}>
+          <Grid item xs={12} md={6}>
+            <SliderSelect />
+            <TenureSelect />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Result />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
