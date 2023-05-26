9940266bf98cddffa45b4448dd1795bda7fa9b52
Sanjay <ksanjayk@protonmail.com>
2023-05-26 19:11:52 +0530

    layout with stack

--- mortage-calculator/src/components/SliderSelect.js
+++ mortage-calculator/src/components/SliderSelect.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import SliderComponent from './common/SliderComponent';
+import { Stack } from '@mui/material';
 
 function SliderSelect() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Stack my={1.4}>
+      <Stack gap={1}>
+        <SliderComponent
+          label='Home Value'
+          min={10}
+          max={250}
+          defaultValue={40}
+          step={10}
+          onChange={(e, value) => console.log(value)}
+          unit={'$'}
+          amount={3000}
+        />
+
+        <SliderComponent
+          label='Down Payment'
+          min={10}
+          max={150}
+          defaultValue={40}
+          step={10}
+          onChange={(e, value) => console.log(value)}
+          unit={'$'}
+          amount={500}
+        />
+        <SliderComponent
+          label='Loan Amount'
+          min={10}
+          max={1250}
+          defaultValue={40}
+          step={10}
+          onChange={(e, value) => console.log(value)}
+          unit={'$'}
+          amount={700}
+        />
+      </Stack>
+    </Stack>
+  );
 }
 
-export default SliderSelect
+export default SliderSelect;
