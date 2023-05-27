9a4490092ed905cc37c9774c60e442748d0c18e3
Sanjay <ksanjayk@protonmail.com>
2023-05-27 19:01:50 +0530

    pie chart

---
+++ mortage-calculator/Learning.md
@@ -0,0 +1,5 @@
+1. In real world , the figma is created which specifies all the style, colour and design etc. The developer will create theme and define the style, color, font , design etc so that the style is consistent throughtout.
+2. In Typography's variant attribite we can define what style a element will take
+3. In Typography's component attribite we can define the component
+4. sx is used to style all the components of mui
+5. container centers the content
--- mortage-calculator/src/components/Result.js
+++ mortage-calculator/src/components/Result.js
@@ -1,10 +1,44 @@
+import { Stack, Typography } from '@mui/material';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
-function Result() {
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function Result({ data }) {
+  const { homeValue, downPayment, loanAmount, loanTerm, interestRate } = data;
+  const totalLoanMonths = loanTerm * 12;
+  const interestPerMonth = interestRate / 100 / 12;
+  const monthlyPayment =
+    (loanAmount *
+      interestPerMonth *
+      (1 + interestPerMonth) ** totalLoanMonths) /
+    ((1 + interestPerMonth) ** totalLoanMonths - 1);
+  const totalInterestGenerated = monthlyPayment * totalLoanMonths - loanAmount;
+
+  const pieChartData = {
+    labels: ['Principle', 'Interest'],
+    datasets: [
+      {
+        label: 'Ration of Principle and Interest',
+        data: [loanAmount, totalInterestGenerated],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <Stack gap={3}>
+      <Typography variant='h5' textAlign='center'>
+        Monthly Payment: $ {monthlyPayment.toFixed(2)}
+      </Typography>
+      <Stack direction={'row'} justifyContent={'center'}>
+        <div>
+          <Pie data={pieChartData} />
+        </div>
+      </Stack>
+    </Stack>
+  );
 }
 
-export default Result
+export default Result;
