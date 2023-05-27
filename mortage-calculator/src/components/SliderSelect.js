009f16eb9fcddf7d9972b1c501fd36c3feeca506
Sanjay <ksanjayk@protonmail.com>
2023-05-27 10:53:39 +0530

    added data management

--- mortage-calculator/src/components/SliderSelect.js
+++ mortage-calculator/src/components/SliderSelect.js
@@ -2,40 +2,51 @@ import React from 'react';
 import SliderComponent from './common/SliderComponent';
 import { Stack } from '@mui/material';
 
-function SliderSelect() {
+function SliderSelect({ setData, data }) {
+  const bank_limit = 10000;
   return (
     <Stack my={1.4}>
       <Stack gap={1}>
         <SliderComponent
           label='Home Value'
           min={10}
-          max={250}
-          defaultValue={40}
+          max={bank_limit}
+          defaultValue={data.homeValue}
           step={10}
-          onChange={(e, value) => console.log(value)}
+          onChange={(e, value) => setData({ ...data, homeValue: value })}
           unit={'$'}
-          amount={3000}
+          amount={data.homeValue}
         />
 
         <SliderComponent
           label='Down Payment'
           min={10}
-          max={150}
-          defaultValue={40}
+          max={data.homeValue}
+          defaultValue={data.downPayment}
           step={10}
-          onChange={(e, value) => console.log(value)}
+          onChange={(e, value) => setData({ ...data, downPayment: value })}
           unit={'$'}
-          amount={500}
+          amount={data.downPayment}
         />
         <SliderComponent
           label='Loan Amount'
           min={10}
-          max={1250}
-          defaultValue={40}
+          max={data.homeValue}
+          defaultValue={data.loanAmount}
           step={10}
-          onChange={(e, value) => console.log(value)}
+          onChange={(e, value) => setData({ ...data, loanAmount: value })}
           unit={'$'}
-          amount={700}
+          amount={data.loanAmount}
+        />
+        <SliderComponent
+          label='Interest Rate'
+          min={2}
+          max={18}
+          defaultValue={data.interestRate}
+          step={0.5}
+          onChange={(e, value) => setData({...data, interestRate:value})}
+          unit={'%'}
+          amount={data.interestRate}
         />
       </Stack>
     </Stack>
